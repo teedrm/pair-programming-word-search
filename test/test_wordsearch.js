06746fa3c0af297efb0599273e7d5c4b4701d6f0
Thy Nguyen <tee@Thys-iMac.local>
2022-11-23 20:44:06 -0700

    completed exercise

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -15,7 +15,7 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'FRANK')
+    ], 'FRANK') // TEST IF WORD EXISTS
 
     assert.isFalse(result);
   });
@@ -31,8 +31,25 @@ describe("#wordSearch()", function() {
       ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
       ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
       ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
-    ], 'SEINFELD')
+    ], 'SEINFELD') // TEST FOR HORIZONTAL 
+
+    assert.isTrue(result);
+  });
+  it("should return true if the word is present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['S', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['Y', 'F', 'C', 'F', 'R', 'U', 'A', 'L'],
+      ['H', 'M', 'J', 'T', 'A', 'V', 'R', 'G'],
+      ['W', 'H', 'C', 'S', 'N', 'E', 'R', 'L'],
+      ['B', 'F', 'R', 'E', 'K', 'E', 'Y', 'B'],
+      ['U', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['E', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'FRANK') // TEST FOR VERTICAL
 
     assert.isTrue(result);
   });
 });
+
+
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,31 @@
-const wordSearch = (letters, word) => { 
+const transpose = function (matrix) {
+let result = [];
+
+for (let i = 0; i < matrix[0].length; i++) {
+    let column = [];
+
+    for (let j = 0; j < matrix.length; j++) {
+        column.push(matrix[j][i]);
+    }
+    result.push(column);
+}
+return result;
+};
+
+const wordHorizontal = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
     for (l of horizontalJoin) {
         if (l.includes(word)) return true
     }
+    return false 
+};
+
+const wordSearch = (letters, word) => {
+    if(wordHorizontal(letters, word) || wordHorizontal(transpose(letters), word)){
+        return true;
+    }
+    return false; 
 }
+
 
 module.exports = wordSearch
